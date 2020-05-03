ad969f02173e852448bc0acdf5c5c757fa4204b7
Rajesh Yadav <rajes.yadav@unthinkable.co>
2020-05-03 22:36:03 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Employee from './Employees/Employee/Employee';
+
+class App extends Component {
+  render() {
+    return (<div >
+    Hello
+    <Employee></Employee>
+  </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/Employees/Employee/Employee.js
@@ -0,0 +1,8 @@
+import React from 'react';  
+
+const Employee = (props) =>
+{
+    return <p>Employee component</p>
+}
+
+export default Employee;
